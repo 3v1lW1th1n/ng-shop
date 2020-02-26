a01b2f94ce2bb9ca679be088be40e730a5aa6eeb
svetlana-b <60920490+svetlana-b@users.noreply.github.com>
2020-02-26 16:32:42 +0200

    Feature/navigate (#47)
    * Feature: delete comments [ref: no-ref]
    
    * Feature: buttons styles change [ref: no-ref]
    
    * Feature: unused files deleted [ref: no-ref]
    
    * Feature: unused styles removed [ref: no-ref]
    
    * Feature: scroll change [ref: no-ref]

--- src/app/router.animation.ts
+++ src/app/router.animation.ts
@@ -1,5 +1,6 @@
 import {
-  animate, AnimationMetadata,
+  animate,
+  AnimationMetadata,
   AnimationTriggerMetadata,
   group,
   query,
@@ -9,37 +10,59 @@ import {
 } from '@angular/animations';
 
 export const rightAnimation: AnimationMetadata[] = [
-  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
+  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+    optional: true,
+  }),
   group([
-    query(':enter', [
-      style({ transform: 'translateX(100%)' }),
-      animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' })),
-    ], { optional: true }),
-    query(':leave', [
-      style({ transform: 'translateX(0%)' }),
-      animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' })),
-    ], { optional: true }),
+    query(
+      ':enter',
+      [
+        style({ transform: 'translateX(100%)' }),
+        animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' })),
+      ],
+      { optional: true },
+    ),
+    query(
+      ':leave',
+      [
+        style({ transform: 'translateX(0%)' }),
+        animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' })),
+      ],
+      { optional: true },
+    ),
   ]),
 ];
-
 
 export const leftAnimation: AnimationMetadata[] = [
-  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), { optional: true }),
+  query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+    optional: true,
+  }),
   group([
-    query(':enter', [
-      style({ transform: 'translateX(-100%)' }),
-      animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' })),
-    ], { optional: true }),
-    query(':leave', [
-      style({ transform: 'translateX(0%)' }),
-      animate('0.5s ease-in-out', style({ transform: 'translateX(100%)' })),
-    ], { optional: true }),
+    query(
+      ':enter',
+      [
+        style({ transform: 'translateX(-100%)' }),
+        animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' })),
+      ],
+      { optional: true },
+    ),
+    query(
+      ':leave',
+      [
+        style({ transform: 'translateX(0%)' }),
+        animate('0.5s ease-in-out', style({ transform: 'translateX(100%)' })),
+      ],
+      { optional: true },
+    ),
   ]),
 ];
 
-export const customTransition: AnimationTriggerMetadata = trigger('routeTransition', [
-  transition('products => product', rightAnimation),
-  transition('product => products', leftAnimation),
-  transition('cart => *', leftAnimation),
-  transition('* => cart', rightAnimation),
-]);
+export const customTransition: AnimationTriggerMetadata = trigger(
+  'routeTransition',
+  [
+    transition('products => product', rightAnimation),
+    transition('product => products', leftAnimation),
+    transition('cart => *', leftAnimation),
+    transition('* => cart', rightAnimation),
+  ],
+);
